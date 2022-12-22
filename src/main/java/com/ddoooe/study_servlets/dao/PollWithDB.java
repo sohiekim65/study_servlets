32e3f05e07c4d815cdb9c6333d832314de4fcf2e
sohiekim65 <sohiekim65@gmail.com>
2022-12-22 11:11:50 +0900

    add commit

--- src/main/java/com/ddoooe/study_servlets/dao/PollWithDB.java
+++ src/main/java/com/ddoooe/study_servlets/dao/PollWithDB.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 import java.util.HashMap;
 
 public class PollWithDB {
-    public void getQuestion(String questionsUid) throws SQLException{
+    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
         Commons commons = new Commons();
         Statement statement = commons.getStatement();
 
@@ -16,11 +16,14 @@ public class PollWithDB {
         ResultSet resultSet = statement.executeQuery(query);
         // 로우 하나를 받아내는 데이터를 결정해야 한다.
         // 넘겨줄 수 있는 건 3가지 중 하나다(지금은 row하나라 hashMap). (bean, hashMap, arrayList)
-        HashMap<String, Object> result;
+        HashMap<String, Object> result = null;
         while(resultSet.next()){
-            resultSet.getString("QUESTIONS_UID");
-            resultSet.getString("QUESTIONS");
-            resultSet.getInt("ORDERS");
+            result = new HashMap<>();
+            result.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
+            result.put("QUESTIONS",resultSet.getString("QUESTIONS"));
+            result.put("ORDERS",resultSet.getInt("ORDERS"));
         }
+
+        return result;
     }
 }
--- src/main/java/com/ddoooe/study_servlets/servlets/DetailServlets.java
+++ src/main/java/com/ddoooe/study_servlets/servlets/DetailServlets.java
@@ -2,7 +2,12 @@ package com.ddoooe.study_servlets.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
 
+import com.ddoooe.study_servlets.dao.PollWithDB;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,14 +18,32 @@ import jakarta.servlet.http.HttpServletResponse;
 public class DetailServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
         // input type
         String questions_Uid = request.getParameter("QUESTIONS_UID");
 
         // biz with DB and Class
+        PollWithDB pollWithDB = new PollWithDB();
+        // function 불러오기, 밖에서 선언
+        HashMap<String, Object> question = null;
+        try {
+            // function 재선언
+            question = pollWithDB.getQuestion(questions_Uid);
+            // question이 동작하는 지 확인용 출력
+            System.out.println(question.get("QUESTIONS_UID"));
+            System.out.println(question.get("QUESTIONS"));
+            System.out.println(question.get("ORDERS"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // output with html
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter printWriter = response.getWriter();
-        printWriter.close();
+        // jsp 입장에서 데이터 받는, 서블릿에서 데이터 넘겨주는
+        request.setAttribute("question", question);
+
+        // jsp로 작동하도록 방향 트는 url 인스턴스화(=printWriter 대체용)
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/polls/details.jsp");
+        // forward하면서 request, response 모두 넘겨주는
+        requestDispatcher.forward(request, response);
     }
 }
